154bec38756d78e9802c970f4330db51ee8db747
Jan Wylegała <jajowa@Jans-MacBook-Air.local>
2022-04-16 18:12:27 +0200

    Built About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,17 @@ import LinkButton from "../components/LinkButton"
 import "./About.scss"
 
 const About = () => {
+  document.body.style.backgroundImage = "url('./images/02d.png')"
+
   return (
     <div>
       <LinkButton name="Home" path="/" />
+      <div className="app-description">
+        <p>
+          This app has been built with React and uses OpenWeather api to pull weather information. <br />
+          Feel free to visit my <a target="_blank" href="https://github.com/jashioq/Weather_App">GitHub profile</a> to check out the code.
+        </p>
+      </div>
     </div>
   )
 }
